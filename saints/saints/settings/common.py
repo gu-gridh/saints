d03d93bb1422de98b420e153f8ff7f77b15b73f0
j4lib <julia.beck@lir.gu.se>
2024-06-26 18:10:10 +0200

    Fix static file handling

--- saints/saints/settings/common.py
+++ saints/saints/settings/common.py
@@ -18,7 +18,7 @@ from pathlib import Path
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/

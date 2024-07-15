4cbdb60c33b2b3c0689b3c4e8c8d4d9460b6b479
j4lib <julia.beck@lir.gu.se>
2024-07-15 16:13:37 +0200

    Adding CORS package.

--- saints/saints/settings/common.py
+++ saints/saints/settings/common.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'rest_framework',
     # 'formset',
     'ckeditor',
+    'corsheaders',
     'django_filters',
     'explore.apps.ExploreConfig',
     'django.contrib.admin',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://saints.dh.gu.se",
+    os.getenv('LOCAL'),
+    os.getenv('LOCAL127'),
 ]
 
 ROOT_URLCONF = 'saints.urls'
